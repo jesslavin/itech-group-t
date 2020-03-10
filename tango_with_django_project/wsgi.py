1da135cdfac239e10c768d5006cc7d2eac0d05c9
Jessica Lavin <2495543l@student.gla.ac.uk>
2020-03-10 17:41:57 +0000

    Revert "migrations/tests removed"
    This reverts commit b91690014554530e73841ec1fe36c2f8042214f0.

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vinyl_destination project.
+WSGI config for tango_with_django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinyl_destination.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 application = get_wsgi_application()
