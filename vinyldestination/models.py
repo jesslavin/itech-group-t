1ad13a51f016759d434b8d3ef2dde09dd4815167
Simon Manton Milne <0005557m@student.gla.ac.uk>
2020-03-24 05:19:08 +0000

    Changes to pop_script, models, admin
    Added artists, shop images, stock

--- vinyldestination/models.py
+++ vinyldestination/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
+
 # class Artist(models.Model):
 #     name = models.CharField(max_length=128, unique=True)
 #     views = models.IntegerField(default=0)
@@ -54,6 +56,7 @@ class Artist(models.Model):
     description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
     slug = models.SlugField(unique=True)
     insta = models.URLField(blank=True)
+    image = models.ImageField(upload_to='images/artists', blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -65,9 +68,11 @@ class Artist(models.Model):
     def __str__(self):
         return self.name
 
+
 class Record(models.Model):
     NAME_MAX_LENGTH = 128
     GENRE_MAX_LENGTH = 16
+    DESCRIPTION_MAX_LENGTH = 1000
     r_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     year = models.IntegerField(default=0, validators=[MaxValueValidator(2020)])
@@ -77,6 +82,7 @@ class Record(models.Model):
     a_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
     genre = models.CharField(max_length=GENRE_MAX_LENGTH)
     image = models.ImageField(upload_to='images/records/', blank=True)
+    description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.a_id) + "-" + slugify(self.name)
@@ -88,7 +94,8 @@ class Record(models.Model):
     def __str__(self):
         return self.name
 
-class Shop (models.Model):
+
+class Shop(models.Model):
     NAME_MAX_LENGTH = 128
     DESCRIPTION_MAX_LENGTH = 1000
 
@@ -98,6 +105,7 @@ class Shop (models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='images/shops/', blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -109,8 +117,25 @@ class Shop (models.Model):
     def __str__(self):
         return self.name
 
-class Review (models.Model):
 
+class Stock(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    stock_item = models.ForeignKey(Record, on_delete=models.CASCADE, blank=True)
+
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.shop) + "-" + slugify(self.stock_item)
+        super(Stock, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Stocks'
+
+    def __str__(self):
+        return self.slug
+
+
+class Review(models.Model):
     TITLE_MAX_LENGTH = 50
     RATING = (
         (1, '1'),
@@ -119,7 +144,7 @@ class Review (models.Model):
         (4, '4'),
         (5, '5')
     )
-#or try to sub get_user_model() with User, if this doesn't work
+    # or try to sub get_user_model() with User, if this doesn't work
     record = models.ForeignKey(Record, on_delete=models.CASCADE)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
@@ -128,6 +153,7 @@ class Review (models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
