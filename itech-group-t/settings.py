f2251733489f6e479091a9522522fe0c3334b6f2
Daniel Mitchell <d_mitch@live.co.uk>
2020-03-31 16:51:14 +0100

    Revert "local host added"
    This reverts commit 619a2fe7120d52987796eccac35cc234aa605919.

--- itech-group-t/settings.py
+++ itech-group-t/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['.itechgroupt.pythonanywhere.com',
-                 'localhost']
+ALLOWED_HOSTS = ['.itechgroupt.pythonanywhere.com']
 
 
 # Application definition
